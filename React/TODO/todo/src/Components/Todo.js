1345e126680a5bb05b86a2de3b1fff98553b8fb8
gupta-riya <reeya211999@gmail.com>
2021-06-12 12:20:25 +0530

    todo app static ui

--- React/TODO/todo/src/Components/Todo.js
+++ React/TODO/todo/src/Components/Todo.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
 
 export default class Todo extends Component{
+    
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            tasks:[{id:1,txt:'Task 1'},{id:2,txt:'Task 2'},{id:3,txt:'Task 3'}],
+            currTask:''
+        }
+    }
+    
+    handleChange = (e) =>{
+        let val = e.target.value;
+        console.log(val);
+        this.setState({currTask:val})
+    }
+    
+    
     render(){
-        const tasks = [{id:1,txt:'Task 1'},{id:2,txt:'Task 2'},{id:3,txt:'Task 3'}];
+        
         return(
             <div>
 
                 <div className = 'input-container'>
-                    <input type = "text"></input>
+                    <input  type = "text"  value = {this.state.currTask} onChange={this.handleChange}></input>
                     <button>Add</button>
                 </div>
                 <div className = 'class-list'>
                     <ul>
                         {
-                            tasks.map(task=>(
+                            this.state.tasks.map(task=>(
                                 <li>
                                     <h1>{task.txt}</h1>
                                     <button>Delete</button>
