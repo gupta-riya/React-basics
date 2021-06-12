ff26349dc2410fd57e356dc36840c2b06b035490
gupta-riya <reeya211999@gmail.com>
2021-06-12 12:52:08 +0530

    delete task function added

--- React/TODO/todo/src/Components/Todo.js
+++ React/TODO/todo/src/Components/Todo.js
@@ -11,16 +11,28 @@ export default class Todo extends Component{
         }
     }
     
+    // handles input value 
     handleChange = (e) =>{
         let val = e.target.value;
-        console.log(val);
         this.setState({currTask:val})
     }
     
+    // add task on submitting
     onSubmit = ()=>{
         let newTaskArr = [...this.state.tasks,{id:this.state.tasks.length+1,txt:this.state.currTask}]
         this.setState({tasks:newTaskArr,currTask:''});
     
+    }
+
+    // on deleting task
+    onDelete = (id) =>
+    {
+        // will return all task not equal to that id
+        let nta = this.state.tasks.filter(task=>{
+            return task.id != id;
+        })
+        this.setState({tasks:nta});
+        
     }
     
     render(){
@@ -38,7 +50,7 @@ export default class Todo extends Component{
                             this.state.tasks.map(task=>(
                                 <li key = {task.id}>
                                     <h1>{task.txt}</h1>
-                                    <button >Delete</button>
+                                    <button onClick = {()=>this.onDelete(task.id)}>Delete</button>
                                 </li>
                             ))
                         }
