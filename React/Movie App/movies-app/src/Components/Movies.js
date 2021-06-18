19219f68c657a5ec607f9b479ebd1171c642d89c
gupta-riya <reeya211999@gmail.com>
2021-06-18 10:42:02 +0530

    added movie data

---
+++ React/Movie App/movies-app/src/Components/Movies.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import {getMovies} from './MovieService'
+
+export default class Movies extends Component {
+    
+    
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            movies: getMovies()
+        }
+    }
+    
+    
+    
+    
+    
+    
+    render() {
+        return (
+            <div className = 'row'>
+                <div className = 'col-3'>
+                    <h1>hello</h1>
+                </div>
+                <div className = 'col-9'>
+                   <input type = "text"></input> 
+                   <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
