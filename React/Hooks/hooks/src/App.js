656b1bce8126685dd98d453233dca3925d3c8348
gupta-riya <reeya211999@gmail.com>
2021-07-03 10:49:33 +0530

    hooks practice

---
+++ React/Hooks/hooks/src/App.js
@@ -0,0 +1,12 @@
+import Us from './Components/Us';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Us/>
+   </>
+  );
+}
+
+export default App;
---
+++ React/Hooks/hooks/src/Components/Us.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+
+function Us() {
+    
+    const [msgObj,setMessage] = useState({message:''});
+    const handleChange = (e) =>{
+        let val = e.target.value;
+        // msgObj.message = val;
+        // console.log(msgObj);
+        // setMessage({...msgObj,message:val});
+        let obj = {message:val};
+        setMessage(obj);
+    }
+    
+    
+    
+    return (
+        
+        <div>
+            <input type = 'text' value={msgObj.message} onChange={handleChange}></input>
+            <p>{msgObj.message}</p>
+        </div>
+    )
+}
+
+export default Us
