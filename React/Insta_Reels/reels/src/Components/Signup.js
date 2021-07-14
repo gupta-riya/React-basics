4ee133da32ad3da2b5f2cffb886744bb69f2ee3e
gupta-riya <reeya211999@gmail.com>
2021-07-14 18:56:37 +0530

    upload profile img in unique user doc

--- React/Insta_Reels/reels/src/Components/Signup.js
+++ React/Insta_Reels/reels/src/Components/Signup.js
@@ -1,5 +1,6 @@
 import React,{useEffect,useState,useContext} from 'react'
 import {AuthContext} from '../Context/AuthProvider';
+import { storage } from '../firebase';
 function Signup() {
 
     const [email,setEmail] = useState('');
@@ -8,16 +9,62 @@ function Signup() {
     const [error,setError] = useState('');
     const [loading,setLoading] = useState(false);
     const {signup} = useContext(AuthContext);
-    console.log(signup);
+    const [file,setFile] = useState(null);
+    // console.log(signup);
     const handleSignup = async (e) =>{
         e.preventDefault();
         setLoading(true);
         let res = await signup(email,password);
         let uid = res.user.uid ;
-        console.log(uid);
+        // console.log(uid);
+        // create user id document and a profileImage folder in it where put the uploaded profile picture
+        const uploadTaskListener = storage.ref(`/users/${uid}/profileImage`).put(file);
+        // fn1 -> progress tracking
+        // fn2 -> error
+        // fn3 -> success
+        uploadTaskListener.on('state_changed',fn1,fn2,fn3);
+        
+        // use to show how much your file is upload or current progress
+        function fn1(snapshot){
+
+            var progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            console.log('Upload is ' + progress + '% done');
+        }
+
+        // any errror occured during uploading
+        function fn2(error)
+        {
+            setError(error);
+            setTimeout(()=>{
+                setError('')
+            },2000);
+            setLoading(false);
+        }
+
+        // promise return function that downloads url of the image
+        async function fn3()
+        {
+            let downloadUrl = await uploadTaskListener.snapshot.ref.getDownloadURL();
+            console.log(downloadUrl);
+        }
+
+
+
         setLoading(false);
 
     }
+
+    // for setting up profile picture at time of signup
+   const handleFileSubmit = (e)=>{
+
+        let file = e.target.files[0];   // as files is an arrya
+        //console.log(file);
+        if(file != null)
+        {
+            setFile(file);
+        }
+
+   }
 
     return (
         <div>
@@ -37,6 +84,10 @@ function Signup() {
                     <input type = 'password' value = {password} onChange = {(e) => setPassword(e.target.value)}/>
                 </div>
 
+                <div>
+                    <label htmlFor='profile'>Profile Image</label>
+                    <input type='file' accept='image/*' onChange={handleFileSubmit}/>
+                </div>
                 <button type = 'submit' disabled={loading}>Login</button>
             </form>
         </div>
