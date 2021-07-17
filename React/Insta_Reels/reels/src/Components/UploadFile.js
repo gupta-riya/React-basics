9485446e5ea5d63604cbec32403e77d966be3a6b
gupta-riya <reeya211999@gmail.com>
2021-07-18 01:58:58 +0530

    upto uploadFile start

--- React/Insta_Reels/reels/src/Components/UploadFile.js
+++ React/Insta_Reels/reels/src/Components/UploadFile.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import LinearProgress  from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => ({
    
@@ -11,12 +13,36 @@ const useStyles = makeStyles((theme) => ({
 function UploadFile() {
 
     const classes = useStyles();
+    const [loading,setLoading] = useState(false);
+    const[error,setError] = useState(null);
+    const types = ['video/mp4','video/webm','video/ogg'];
     return (
-        <div>
-            <Button variant="outlined" color="secondary">
-                Upload File
+
+        <>
+        {
+            
+            error!=null?<Alert severity="error">{error}</Alert>:<>
+            <input
+            color = 'primary'
+            type='file'
+            id='icon-button-file'
+            style={{display:'none'}}
+            />
+            <label htmlFor = 'icon-button-file'>
+            <Button disabled={loading} variant="outlined" component='span' className={classes.button} size='medium' color="secondary">
+               Upload File
             </Button>
-        </div>
+            </label>
+            {loading?<LinearProgress color='secondary' style={{marginTop:'6%'}} />:<></>}
+            </>
+        }
+        </>
+
+        // <div>
+        //     <Button variant="outlined" color="secondary">
+        //         Upload File
+        //     </Button>
+        // </div>
     )
 }
 
