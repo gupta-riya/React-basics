665f7911ff05b30cdcc7e87c347be298199a8af0
gupta-riya <reeya211999@gmail.com>
2021-08-13 03:10:39 +0530

    recipe app started

---
+++ React/Recipe App/recipe_app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './MyComponents/Header'
+import FoodChart from './MyComponents/FoodChart'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <FoodChart/>
+    </>
+  );
+}
+
+export default App;
---
+++ React/Recipe App/recipe_app/src/MyComponents/FoodChart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './foodChart.css'
+
+function FoodChart() {
+    return (
+
+        <div>
+            <h1>Main page</h1>
+        </div>
+    )
+}
+
+export default FoodChart
