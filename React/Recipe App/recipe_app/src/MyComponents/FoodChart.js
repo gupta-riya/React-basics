813a0f620125ec0989fba78a54b18abc588017f9
gupta-riya <reeya211999@gmail.com>
2021-08-15 08:00:52 +0530

    modal created

--- React/Recipe App/recipe_app/src/MyComponents/FoodChart.js
+++ React/Recipe App/recipe_app/src/MyComponents/FoodChart.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react'
 import './foodChart.css'
 import FoodLayout from './FoodLayout'
-import {recipeData} from './recipeData'
 
 
 export default class FoodChart extends Component
--- React/Recipe App/recipe_app/src/MyComponents/FoodItem.js
+++ React/Recipe App/recipe_app/src/MyComponents/FoodItem.js
@@ -6,6 +6,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 import { Button } from '@material-ui/core';
+import Modal from '@material-ui/core/Modal';
 // import { recipeData } from './recipeData';
 
 const useStyles = makeStyles((muiBaseTheme) => ({ 
@@ -41,7 +42,46 @@ const useStyles = makeStyles((muiBaseTheme) => ({
       backgroundColor:"#ffdb68"
     },
     padding: `${muiBaseTheme.spacing(0.5)}px ${muiBaseTheme.spacing(1)}px` 
+  },
+  paper: {
+    position: 'absolute',
+    width: 400,
+    maxHeight: '80vh',
+    left: '50%',
+    top: '40%',
+    transform: `translate(-50%, -40%)`,
+    backgroundColor: 'whitesmoke',
+    boxShadow: muiBaseTheme.shadows[5],
+    padding: muiBaseTheme.spacing(2, 4, 3),
+    overflow: 'auto'
+  },
+  modal_title : {
+    
+  },
+  proc_item_button : {
+    
+    color : '#8e969e',
+    fontSize : '1rem',
+    marginRight : '5rem'
+  },
+
+  active : {
+    color : '#fedc56',
+    fontSize : '1rem',
+    marginRight : '5rem'
+  },
+  
+  special_bullet : {
+    color : '#be5504'
+  },
+  li_text : {
+    color : 'black'
   }
+
+
+
+   
+    
  
 }));
 
@@ -50,6 +90,62 @@ export default function FoodItem(props)
     
       const classes = useStyles();
       const recipe = props.recipe;
+      const [open,setOpen] = React.useState(false);
+      const [bgIngButton,setIngActive] = React.useState(classes.active);
+      const [bgProcButton,setProcActive] = React.useState(classes.proc_item_button);
+      const [content,setContent] = React.useState(recipe.ingredients);
+
+
+      const handleOpen = () => {
+        setOpen(true);
+      }
+
+      const handleClose = () => {
+        setOpen(false);
+      }
+
+      const handleRecipe = (e) => {
+
+        let id = e.target.parentNode.getAttribute("id");
+        
+        if(id === '1')
+        {
+          setIngActive(classes.active);
+          setProcActive(classes.proc_item_button);
+          setContent(recipe.ingredients);
+        }
+        else
+        {
+          setProcActive(classes.active);
+          setIngActive(classes.proc_item_button);
+          setContent(recipe.procedure);
+        }
+
+
+
+      }
+
+      const body = (
+        <div className={classes.paper}>
+          <h2 id="simple-modal-title" className = {classes.modal_title}>{recipe.name}</h2>
+          <div className = {classes.proc_buttons}>
+            <Button className = {bgIngButton} id = '1' onClick = {handleRecipe}>Ingredients</Button>
+            <Button className = {bgProcButton} id = '2' onClick = {handleRecipe}>Procedure</Button>
+          </div>
+          
+            <ul className = {classes.special_bullet} >
+              {
+                content.map((cont) => {
+                  return <li><span className = {classes.li_text}>{cont}</span></li>
+                })
+              }
+            </ul>
+         
+          
+        </div>
+      );
+
+
       return(
       
           <>
@@ -69,9 +165,17 @@ export default function FoodItem(props)
                         >
                             {recipe.name}
                         </Typography>
-                        <Button variant="contained" color="primary" className={classes.recipe_button}>
+                        <Button variant="contained" color="primary" className={classes.recipe_button} onClick={handleOpen}>
                           Recipe
                         </Button>
+                        <Modal
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="simple-modal-title"
+                          aria-describedby="simple-modal-description"
+                        >
+                          {body}
+                        </Modal>
                         <Divider className={classes.divider} light />
                         
                         </CardContent>
@@ -80,3 +184,5 @@ export default function FoodItem(props)
           </>
       )
 }
+
+
