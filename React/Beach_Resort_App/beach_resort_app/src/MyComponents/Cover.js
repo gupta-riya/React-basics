32b37846b4dcfe203a96e6c40f205185c8522264
gupta-riya <reeya211999@gmail.com>
2021-08-19 02:24:19 +0530

    cover page section

--- React/Beach_Resort_App/beach_resort_app/src/MyComponents/Cover.js
+++ React/Beach_Resort_App/beach_resort_app/src/MyComponents/Cover.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import './cssStyles/cover.css'
 
-function Cover() {
+function Cover(props) {
 
-    // const style = {
-        
-    // }
-
-
+    let url = `http://localhost:3000/Images/`;
+    let  img_url = url + props.img;
+    
     return (
-        <div className = "main-container">
+        <div style={{ background: `url(${img_url}) 50%/cover no-repeat`}} className = "main-container">
 
             <div className = "sub-container">
                 <h1 className="main-heading">
-                    Luxurious Rooms
+                    {props.head}
                 </h1>
                 <div className = "divider"></div>
-                <h2>Deluxe Rooms Starting At ₹1000* Only</h2>
-                <button className = "cover-button">OUR ROOMS</button>
+                <h2>{props.desc}</h2>
+                <button className = "cover-button">{props.btn_txt}</button>
             </div>
 
 
--- React/Beach_Resort_App/beach_resort_app/src/MyComponents/Home.js
+++ React/Beach_Resort_App/beach_resort_app/src/MyComponents/Home.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Cover from './Cover.js'
 
 function Home() {
+
+    const head = "Luxurious Rooms";
+    const desc = "Deluxe Rooms Starting At ₹1000* Only";
+    const btn_txt = "OUR ROOMS";
+    const img = "defaultBcg.jpeg";
+
     return (
         
         <>
           
-            <Cover/>
+            <Cover head = {head} desc={desc} btn_txt={btn_txt} img = {img}/>
 
         </>
         
