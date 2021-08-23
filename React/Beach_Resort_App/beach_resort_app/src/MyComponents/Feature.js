7de1673caea33ba0a2163f06edeb5fc45a5dbe91
gupta-riya <reeya211999@gmail.com>
2021-08-23 21:03:58 +0530

    home page complete

--- React/Beach_Resort_App/beach_resort_app/src/MyComponents/Feature.js
+++ React/Beach_Resort_App/beach_resort_app/src/MyComponents/Feature.js
@@ -6,13 +6,15 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+   
   },
   paper: {
-    padding: theme.spacing(1),
     textAlign: 'center',
-    backgroundColor: 'transparent',
-    border : '1px solid red',
-    color : 'black'
+    color : 'black',
+    margin : '1rem 20px',
+    minWidth : '300px',
+    boxShadow: '2px 5px 3px 0 rgb(0 0 0 / 50%)'
+    
   },
   service_icon : {
       color :' rgb(177, 126, 33)',
@@ -27,7 +29,79 @@ const useStyles = makeStyles((theme) => ({
   desc : {
     fontSize :'1rem',
     padding : '0 1rem'
-  }
+  },
+  img_container : {
+      width: '100%',
+      height : '90%',
+      position:'relative',
+      backgroundColor : '#c0c0c0',
+      
+    '&:hover' : {
+        background: 'rgba(0, 0, 0, 0.8)',
+        "& $bc_img" : {
+            opacity : 0.3,
+        },
+        "& $price_info" : {
+            opacity : 0
+        },
+        "& $feature_button" : {
+            transform : 'scale(1)'
+        }
+    }
+  },
+  bc_img : {
+      width : '100%',
+      height : '100%',
+  },
+  price_info : {
+     width : '50px',
+     position : 'absolute',
+     top : '0',
+     left : '0',
+    //  zIndex : '1',
+     backgroundColor : 'black',
+     color : 'white',
+     padding : '5px 10px 5px 5px',
+     borderRadius : '0 50% 50% 0',
+     display: 'block'
+  },
+  price : {
+    margin : '0rem 0'
+  },
+  p_ngt : {
+    margin : '0rem 0',
+    fontSize : '0.6rem'
+  },
+  room_info : {
+      padding : '0.8rem 0',
+      fontSize : '1.2rem',
+      fontWeight : '700',
+      backgroundColor:'#c0c0c0',
+     
+  },
+  feature_button : {
+    padding: '0.4rem 1rem',
+    fontSize: '1.1rem',
+    color: 'black',
+    border: '3px solid transparent',
+    backgroundColor: 'rgb(197, 162, 97)',
+    position : 'absolute',
+    top: '35%',
+    left: '35%',
+    translate : '(-35%, -35%)',
+    transform:'scale(0)',
+    transition: 'all 0.3s linear',
+
+     '&:hover': {
+        display: 'inline-block',
+        border: '3px solid rgb(197, 162, 97)',
+        background: 'transparent',
+        color: 'rgb(197, 162, 97)'
+     }
+}
+
+
+
 }));
 
 
@@ -38,14 +112,15 @@ export default function Feature() {
   function FormRow() {
     return (
       <React.Fragment>
-        <Grid item xs={12} sm ={6} md={4} >
+        <Grid item sm={12} md ={6} lg={4} >
           <div className={classes.paper}>
               <div className = {classes.img_container}>
-                  <img className = {classes.bc_img} src = "" alt = "bc_img"/> 
+                  <img className = {classes.bc_img} src = "./Images/room-8.jpeg" alt = "bc_img"/> 
                   <div className = {classes.price_info}>
-                      <h6>$ 400</h6>
-                      <p>per night</p>
+                      <h4 className = {classes.price}>$ 400</h4>
+                      <p className = {classes.p_ngt}>per night</p>
                   </div>
+                  <button className = {classes.feature_button}>FEATURES</button>
               </div>
               <div className = {classes.room_info}>
                   Double Deluxe
@@ -53,13 +128,35 @@ export default function Feature() {
                 
           </div>
         </Grid>
-        <Grid item xs={12} sm ={6} md={4}>
+        <Grid item sm={12} md ={6} lg={4}>
           <div className={classes.paper}>
+            <div className = {classes.img_container}>
+                  <img className = {classes.bc_img} src = "./Images/room-12.jpeg" alt = "bc_img"/> 
+                  <div className = {classes.price_info}>
+                      <h4 className = {classes.price}>$ 500</h4>
+                      <p className = {classes.p_ngt}>per night</p>
+                  </div>
+                  <button className = {classes.feature_button}>FEATURES</button>
+              </div>
+              <div className = {classes.room_info}>
+                  Family Deluxe
+              </div>
            
           </div>
         </Grid>
-        <Grid item xs={12} sm ={6} md={4}>
+        <Grid item sm={12} md ={6} lg={4}>
           <div className={classes.paper}>
+            <div className = {classes.img_container}>
+                <img className = {classes.bc_img} src = "./Images/details-1.jpeg" alt = "bc_img"/> 
+                <div className = {classes.price_info}>
+                    <h4 className = {classes.price}>$ 600</h4>
+                    <p className = {classes.p_ngt}>per night</p>
+                </div>
+                <button className = {classes.feature_button}>FEATURES</button>
+            </div>
+            <div className = {classes.room_info}>
+                Presidential
+            </div>
             
           </div>
         </Grid>
