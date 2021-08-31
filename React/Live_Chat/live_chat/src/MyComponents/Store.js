f7b31aa52082e2fab81ffe38ec6a47a544cf6605
gupta-riya <reeya211999@gmail.com>
2021-08-31 23:02:07 +0530

    store complete

---
+++ React/Live_Chat/live_chat/src/MyComponents/Store.js
@@ -0,0 +1,76 @@
+import React from 'react' 
+
+const ctx = React.createContext();
+
+/*
+
+    {
+        fron: 'user'
+        msg: 'hi'
+        topic : 'general'
+    }
+
+
+    state {
+        topic1: [
+            {msg}, {msg}, {msg}
+        ]
+        topic2: [
+
+        ]
+    }
+
+*/
+
+const initState= {
+
+    general : [
+        {from:'naruto',msg:'hello'},
+        {from:'sakura',msg:'hello'},
+        {from:'sauske',msg:'hello'}
+    ],
+    topic2 : [
+        {from:'lee',msg:'heyyy'},
+        {from:'hinata',msg:'whats up'},
+        {from:'kakashi',msg:'whats up'}
+    ]
+
+}
+
+
+
+function reducer(state,action)
+{
+    const {from,msg,topic} = action.payload;
+    switch(action.type){
+        case 'RECEIVE_MESSAGE':
+            return{
+                ...state,
+                [topic]:[
+                    ...state[topic],
+                    {
+                        from ,
+                        msg 
+                    }
+                ]
+            }
+        default:
+            return state
+    }
+}
+
+export default function Store(props){
+
+
+    const reducerHook = React.useReducer(reducer,initState);
+
+    return (
+
+        <ctx.Provider value={reducerHook}>
+            {props.children}
+        </ctx.Provider>
+    )
+
+
+
+}
