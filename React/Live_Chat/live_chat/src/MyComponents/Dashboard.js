5796e0f037f9bdf2ae024b6a2df0e748a31f5ebb
gupta-riya <reeya211999@gmail.com>
2021-08-31 23:44:40 +0530

    dashboard complete

--- React/Live_Chat/live_chat/src/MyComponents/Dashboard.js
+++ React/Live_Chat/live_chat/src/MyComponents/Dashboard.js
@@ -8,7 +8,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import {ctx} from './Store'
 
 
 
@@ -44,6 +44,9 @@ const useStyles = makeStyles(theme => ({
     },
     textField:{
         width:'90%'
+    },
+    chatItem:{
+        marginBottom:'1rem'
     }
 }))
 
@@ -52,6 +55,14 @@ export default function Dashboard()
 {
     const classes = useStyles();
 
+    // context
+    const [allChats] = React.useContext(ctx);
+    // console.log({allChats});
+
+    const topics = Object.keys(allChats);
+    
+    //local state
+    const [activeTopic,changeActiveTopic] = React.useState(topics[0]);
     const [textValue, changeTextValue] = React.useState('');
 
     return(
@@ -62,16 +73,16 @@ export default function Dashboard()
                     Chat App
                 </Typography>
                 <Typography component="h5">
-                    Topic Placeholder
+                    {activeTopic}
                 </Typography>
 
                 <div className={classes.flex}>
                     <div className={classes.topicWindow}>
                         <List>
                             {
-                                ['General'].map((topic)=>(
+                                topics.map((topic)=>(
                                    
-                                        <ListItem button>
+                                        <ListItem onClick = {(e)=>changeActiveTopic(e.target.innerText)}  key={topic} button >
                                             <ListItemText primary={topic} />
                                         </ListItem>
                                     
@@ -81,11 +92,11 @@ export default function Dashboard()
                     </div>
                     <div className={classes.chatWindow}>
                         {
-                            [{from:'user',msg:'hello'}].map((chat,idx)=>(
+                            allChats[activeTopic].map((chat,idx)=>(
                             
-                                <div className={classes.flex}>
+                                <div className={`${classes.flex} ${classes.chatItem}`}>
                                     <Chip label={chat.from} className={classes.chip} key={idx} />
-                                    <Typography varient='p'>{chat.msg}</Typography>
+                                    <Typography varient='body1' gutterBottom>{chat.msg}</Typography>
                                 </div>
                                 
                                 
--- React/Live_Chat/live_chat/src/MyComponents/Store.js
+++ React/Live_Chat/live_chat/src/MyComponents/Store.js
@@ -1,6 +1,6 @@
 import React from 'react' 
 
-const ctx = React.createContext();
+export const ctx = React.createContext();
 
 /*
 
