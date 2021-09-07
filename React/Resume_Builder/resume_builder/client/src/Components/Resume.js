5954fa359f42fe87e780db809231e24bbdba6000
gupta-riya <reeya211999@gmail.com>
2021-09-07 14:06:42 +0530

    navbar and resume fields

---
+++ React/Resume_Builder/resume_builder/client/src/Components/Resume.js
@@ -0,0 +1,74 @@
+
+import React,{Component} from 'react'
+import Profile from './Profile'
+
+export default class Resume extends Component{
+
+    state = {
+        step: 1,
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        github: '',
+        linkedin: '',
+        facebook: '',
+        instagram: ''
+     }
+
+     nextStep = () => {
+        const {step} = this.state;
+        this.setState ({
+            step: step + 1,
+        });
+    };
+    
+    prevStep = () => {
+        const {step} = this.state;
+        this.setState ({
+          step: step - 1,
+        });
+    };
+    
+    handleChange = ({target: {value, name}}) => {
+        this.setState ({[name]: value});
+    };
+
+
+    render(){
+
+        const {
+            firstname,
+            lastname,
+            email,
+            phone,
+            github,
+            linkedin,
+            facebook,
+            instagram
+        }  = this.state;
+
+
+        const values = {
+            firstname,
+            lastname,
+            email,
+            phone,
+            github,
+            linkedin,
+            facebook,
+            instagram
+        }
+
+        return(
+            <Profile 
+                nextStep={this.nextStep}
+                handleChange= {this.handleChange}
+                values={values}/>
+        )
+
+
+
+    }
+
+}
